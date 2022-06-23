4cede837c4a37ce4fcfa66ad6ba20008e7d8a14a
gratien <gracian2020@gmail.com>
2022-06-23 09:59:37 +0200

    added page not found component

---
+++ web/src/views/PageNotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="notFound">
+    <h1>Page Not Found</h1>
+  </div>
+</template>
+<style>
+.notFound {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+}
+
+h1 {
+  font-weight: bold;
+}
+</style>
