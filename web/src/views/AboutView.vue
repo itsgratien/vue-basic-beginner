9f84ff33e308afb20f0304e56491c73dc4ff03d0
gratien <gracian2020@gmail.com>
2022-06-23 09:36:45 +0200

    modified about view

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <span>Welcome to about page</span>
   </div>
 </template>
 
@@ -10,6 +11,8 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    flex-direction: column;
+    justify-content: center;
   }
 }
 </style>
