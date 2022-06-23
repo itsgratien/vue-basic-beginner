c8f481947d0fbe5ff48bfa049b494a9fdc2b306a
gratien <gracian2020@gmail.com>
2022-06-23 10:13:18 +0200

    added count functionality & change state

--- web/src/components/TheWelcome.vue
+++ web/src/components/TheWelcome.vue
@@ -5,5 +5,25 @@ import WelcomeItem from "@/components/WelcomeItem.vue";
 <template>
   <WelcomeItem>
     <h5>John Doe</h5>
+    <div>
+      <span style="display: block">{{ count }}</span>
+      <button type="button" v-on:click="handleCount">count</button>
+    </div>
   </WelcomeItem>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 1,
+    };
+  },
+  methods: {
+    handleCount(event) {
+      event.preventDefault();
+      this.count += 1;
+    },
+  },
+};
+</script>
